d6a0af179ad4ad0aac309cb9853174f48824bb07
huzidev <huzaifa.iqdev@gmail.com>
2023-05-10 12:00:59 +0500

    initial commit

---
+++ client/src/components/App.tsx
@@ -0,0 +1,11 @@
+import Form from "./Form"
+
+export default function App() {
+  return (
+    <div> 
+      Stable-diffsuion
+      <Form />
+    </div>
+  )
+}
+  
---
+++ client/src/components/Form.tsx
@@ -0,0 +1,38 @@
+import { DownOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Dropdown, Input, Space } from "antd";
+
+export default function Form() {
+  const { TextArea } = Input;
+
+  const items: MenuProps["items"] = [
+    {
+      label: <a href="https://www.antgroup.com">1st menu item</a>,
+      key: "0",
+    },
+    {
+      label: <a href="https://www.aliyun.com">2nd menu item</a>,
+      key: "1",
+    },
+    {
+      type: "divider",
+    },
+    {
+      label: "3rd menu item",
+      key: "3",
+    },
+  ];
+  return (
+    <div>
+       <Dropdown menu={{ items }} trigger={["click"]}>
+         <a onClick={(e) => e.preventDefault()}>
+           <Space>
+             Click me
+             <DownOutlined />
+           </Space>
+         </a>
+       </Dropdown>
+       <TextArea rows={4} />
+    </div>
+  )
+}
