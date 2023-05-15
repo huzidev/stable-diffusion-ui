c537b1f9cb190c32cc62d2f612e6fbf24ed207df
huzidev <huzaifa.iqdev@gmail.com>
2023-05-15 15:09:04 +0500

    json config data

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
+import axios from "axios";
 import bodyParser from "body-parser";
 import cors from "cors";
 import express, { Express, Request, Response } from 'express';
+import fs from "fs";
+import path from "path";
 const server: Express  = express();
 
 const port = 8080;
@@ -18,46 +21,46 @@ server.get("/get", (req: Request, res: Response) => {
 })
 
 server.post("/test", (req: Request, res: Response) => {
-    // const resp = req.body;
-    console.log("req body", req.body);
-    // var data = JSON.stringify({
-    //     "prompt": prompts,
-    //     "steps": 30,
-    //     "cfg_scale": 7,
-    //     "sampler_name": "Heun",
-    //     "batch_size": 1,
-    //     "width": 512,
-    //     "height": 512,
-    //     "seed": -1,
-    // });
+    const { prompt } = req.body;
+    console.log("resp", prompt);
+    var data = JSON.stringify({
+        prompt,
+        "steps": 30,
+        "cfg_scale": 7,
+        "sampler_name": "Heun",
+        "batch_size": 1,
+        "width": 512,
+        "height": 512,
+        "seed": -1,
+    });
     
-    // const config = {
-    //     method: 'post',  
-    //     url: 'http://127.0.0.1:7860/sdapi/v1/txt2img',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     data: data
-    // };
+    const config = {
+        method: 'post',  
+        url: 'http://127.0.0.1:7860/sdapi/v1/txt2img',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
     
     
-    // async function gen() {
-    //     try {
-    //         const result = await axios(config);
-    //         const { images, info } = result.data;
-    //         const filename = Date.now();
-    //         for (const image of images) {
-    //             const buffer = Buffer.from(image, "base64");
-    //             const imgPath = path.join(`images/`, `${filename}.png`);
-    //             fs.writeFileSync(imgPath, buffer);
-    //         }
-    //         console.log("Res", result);
-    //     } catch (e) {
-    //         console.log("Error", e);
+    async function gen() {
+        try {
+            const result = await axios(config);
+            const { images, info } = result.data;
+            const filename = Date.now();
+            for (const image of images) {
+                const buffer = Buffer.from(image, "base64");
+                const imgPath = path.join(`images/`, `${filename}.png`);
+                fs.writeFileSync(imgPath, buffer);
+            }
+            console.log("Res", result);
+        } catch (e) {
+            console.log("Error", e);
             
-    //     }
-    // }
-    // console.log("data", data);
+        }
+    }
+    console.log("data", data);
     res.status(200).send({ message: "Image generated succescully!" });
 })
 
