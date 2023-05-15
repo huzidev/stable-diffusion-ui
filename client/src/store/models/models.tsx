86d28e0439124b215107d7e81a690544dc4d4cef
huzidev <huzaifa.iqdev@gmail.com>
2023-05-15 19:22:48 +0500

    try catch

--- client/src/store/models/models.tsx
+++ client/src/store/models/models.tsx
@@ -1,5 +1,10 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const getModels = createAsyncThunk("models/getModels", async () => {
-    
+    try {
+        
+    } catch (e) {
+        console.log("Error", e);
+        
+    }
 })
