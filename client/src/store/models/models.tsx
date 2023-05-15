62c1fb5420d1d20576a56ddafdb059cc2b816bc6
huzidev <huzaifa.iqdev@gmail.com>
2023-05-15 19:24:10 +0500

    axios resp

--- client/src/store/models/models.tsx
+++ client/src/store/models/models.tsx
@@ -1,10 +1,10 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 export const getModels = createAsyncThunk("models/getModels", async () => {
     try {
-        
+        const res = await axios("http://localhost:8080/models");
     } catch (e) {
         console.log("Error", e);
-        
     }
 })
