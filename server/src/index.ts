ad3a3bc046f087a88f8c0d99a0ac89a5ad06f34d
huzidev <huzaifa.iqdev@gmail.com>
2023-05-15 12:17:05 +0500

    backend linked

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,55 @@
-console.log("Hello world");
+import axios from "axios";
+import cors from "cors";
+import express, { Request, Response } from 'express';
+const server = express();
+
+const port = 8080;
+
+server.use(cors({
+    origin: "*"
+}));
+
+var data = JSON.stringify({
+    "prompt": "village in winter season",
+    "steps": 5,
+    "cfg_scale": 7,
+    "sampler_name": "Heun",
+    "batch_size": 1,
+    "width": 512,
+    "height": 512,
+    "seed": -1,
+});
+
+const config = {
+    method: 'post',
+    url: 'http://127.0.0.1:7860/sdapi/v1/txt2img',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    data: data
+};
+
+server.use(cors({
+    origin: "*"
+}));
+
+async function gen() {
+    const result = await axios(config);
+    console.log("Res", result);
+}
+
+server.get("/get", (req: Request, res: Response) => {
+    res.status(200).send("HEllo");
+})
+
+server.post("/test", (req: Request, res: Response) => {
+    const data = req.body;
+    console.log("data", data);
+    gen();
+    res.status(200).send("Image generated succescully!")
+})
+
+
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+})
