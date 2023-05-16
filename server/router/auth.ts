f9d0dc9af9e16bf73c919a3cffcf699a338296f0
huzidev <huzaifa.iqdev@gmail.com>
2023-05-16 19:52:16 +0500

    res json

--- server/router/auth.ts
+++ server/router/auth.ts
@@ -22,7 +22,7 @@ router.get("/models", (req: Request, res: Response) => {
         console.log("newArr", newArr);
     }
     getModels();
-    res.status(201).send({ message: "Models fetched successfully!" });
+    res.status(201).json({ message: "Models fetched successfully!" });
     } catch (e) {
         console.log("Error", e);
     }
@@ -47,7 +47,7 @@ router.get("/methods", (req: Request, res: Response) => {
             console.log("Samplers", newArr);
         }
         getMethods();
-        res.status(202).send({ message: "Sample methods fetched successfully!" });
+        res.status(202).json({ message: "Sample methods fetched successfully!" });
     } catch (e) {
         console.log("Error", e);
     }
