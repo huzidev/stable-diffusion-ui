fcd5a1cbd81847c2fcfc0728a6c6c2b373a3b2d0
huzidev <huzaifa.iqdev@gmail.com>
2023-05-16 22:00:26 +0500

    map for samplers

--- server/router/auth.ts
+++ server/router/auth.ts
@@ -1,28 +1,20 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import express, { Request, Response } from 'express';
 
 const router = express.Router();
 
-router.get("/models", (req: Request, res: Response) => {
+router.get("/models", async (req: Request, res: Response) => {
     try {
-        let newArr: any = [];
-        const config = {
-            method: 'get',  
-            url: 'http://127.0.0.1:7860/sdapi/v1/sd-models',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        };
-    async function getModels() {
-        const res = await axios(config);
-        let models = res.data;
-        models.forEach((obj: any) => {
-            newArr.push(obj.model_name);
-        });
-        console.log("newArr", newArr);
-    }
-    getModels();
-    res.status(201).send({ message: "Models fetched successfully!" });
+    const config = {
+        method: 'get',  
+        url: 'http://127.0.0.1:7860/sdapi/v1/sd-models',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    const res = await axios(config);
+    const newArr = res.data.map((obj) => obj.model_name)
+    console.log("Models", newArr);
     res.json(newArr);
     } catch (e) {
         console.log("Error", e);
@@ -38,9 +30,9 @@ router.get("/methods", async (req: Request, res: Response) => {
                 'Content-Type': 'application/json'
             }
         };
-        const res = await axios(config);
+        const res: AxiosResponse<string[]> = await axios(config);
         let samples = res.data;
-        const newArr: AxiosResponse<[]> = samples.map((obj) => obj.name);
+        const newArr = samples.map((obj) => obj.name);
         console.log("Samplers", newArr);
         res.json(newArr)
         // res.status(202).send({ message: "Sample methods fetched successfully!" });
