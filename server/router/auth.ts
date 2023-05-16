22402b2617c49cc3555c151e513f7fe8e1d9333a
huzidev <huzaifa.iqdev@gmail.com>
2023-05-16 19:24:32 +0500

    auth server

---
+++ server/router/auth.ts
@@ -0,0 +1,114 @@
+import axios from "axios";
+import bodyParser from "body-parser";
+import cors from "cors";
+import express, { Express, Request, Response } from 'express';
+import fs from "fs";
+import path from "path";
+const server: Express  = express();
+
+server.use(cors({
+    origin: "*"
+}));
+
+server.use(express.json());
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended : true }));
+
+let latestImage: any = "";
+server.post("/generate", (req: Request, res: Response) => {
+    const { prompt } = req.body;
+    console.log("resp", prompt);
+    var data = JSON.stringify({
+        prompt,
+        "steps": 20,
+        "cfg_scale": 7,
+        "sampler_name": "Heun",
+        "batch_size": 1,
+        "width": 512,
+        "height": 512,
+        "seed": -1,
+    });
+    
+    const config = {
+        method: 'post',  
+        url: 'http://127.0.0.1:7860/sdapi/v1/txt2img',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+    async function gen() {
+        try {
+            const result = await axios(config);
+            const { images, info } = result.data;
+            const filename = Date.now();
+            for (const image of images) {
+                const buffer = Buffer.from(image, "base64");
+                const imgPath = path.join(`images`, `${filename}.png`);
+                fs.writeFileSync(imgPath, buffer);
+                latestImage = `${filename}.png`;
+                console.log("latest image name", latestImage);
+            }
+        } catch (e) {
+            console.log("Error", e);
+            
+        }
+    }
+    gen();
+    res.status(200).send({ message: "Image generated succescully!" });
+})
+
+server.get("/latest-img", (req: Request, res: Response) => {
+    res.status(202).json({ image: latestImage })
+})
+
+server.use("/images", express.static("images"));
+
+server.get("/models", (req: Request, res: Response) => {
+    try {
+        const config = {
+            method: 'get',  
+            url: 'http://127.0.0.1:7860/sdapi/v1/sd-models',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+    async function getModels() {
+        const res = await axios(config);
+        let models = res.data;
+        let newArr: any = [];
+        models.forEach((obj: any) => {
+            newArr.push(obj.model_name);
+        });
+        console.log("newArr", newArr);
+    }
+    getModels();
+    } catch (e) {
+        console.log("Error", e);
+    }
+})
+
+
+server.get("/methods", (req: Request, res: Response) => {
+    try {
+        const config = {
+            method: 'get',  
+            url: 'http://127.0.0.1:7860/sdapi/v1/samplers',
+            headers: {  
+                'Content-Type': 'application/json'
+            }
+        };
+        async function getMethods() {
+            const res = await axios(config);
+            let samples = res.data;
+            let newArr = [];
+            samples.forEach((obj) => {
+                newArr.push(obj.name);
+            })
+            console.log("Samplers", newArr);
+        }
+        getMethods()
+    } catch (e) {
+        console.log("Error", e);
+    }
+})
