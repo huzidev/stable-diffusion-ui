ff846b917da95dd75c957f976af842346d7cfb44
huzidev <huzaifa.iqdev@gmail.com>
2023-05-17 13:51:13 +0500

    json stringify types

--- client/src/store/form/actions.ts
+++ client/src/store/form/actions.ts
@@ -1,27 +1,29 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import * as endpoints from "./endpoints";
+import { DataType } from "./types";
 
 export const generateImg = createAsyncThunk(endpoints.FORM, async (prompt: any) => {
     const { prompts } = prompt;
     try {
-        var data = JSON.stringify({
-            "prompt": prompts
-            // "steps": 20,
+        var data: DataType = {
+            "prompt": prompts,
+            // "steps": 5,
             // "cfg_scale": 7,
             // "sampler_name": "Heun",
             // "batch_size": 1,
             // "width": 512,
             // "height": 512,
             // "seed": -1,
-        });
+        };
+        var jsonString = JSON.stringify(data);
         const config = {
             method: 'post',
             url: endpoints.API,
             headers: {
                 'Content-Type': 'application/json'
             },
-            data: data
+            data: jsonString
         };
         await axios(config);
         console.log("generating image");
