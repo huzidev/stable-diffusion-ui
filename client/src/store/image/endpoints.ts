72d191407220693f42cfb179da1e343f366b56a9
huzidev <huzaifa.iqdev@gmail.com>
2023-05-17 13:24:15 +0500

    ts endpoints

--- client/src/store/image/endpoints.ts
+++ client/src/store/image/endpoints.ts
@@ -1,5 +1,2 @@
+export const HOST = "http://localhost:8080/";
 export const IMAGE = "images/get_image";
-export const SIGN_UP = '/auth/v1/sign_up';
-export const SIGN_OUT = '/auth/v1/sign_out';
-
-export const USER_DETAILS = '/user/v1/get/me';
