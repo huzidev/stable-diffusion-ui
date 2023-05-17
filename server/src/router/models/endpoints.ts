3848ffe67b8037f730c731906527fad9c0e93b96
huzidev <huzaifa.iqdev@gmail.com>
2023-05-17 20:22:41 +0500

    models for backend

--- server/src/router/models/endpoints.ts
+++ server/src/router/models/endpoints.ts
@@ -1,1 +1,2 @@
 export const API = "http://127.0.0.1:7860/sdapi/v1/sd-models";
+export const MODEL = "/models";
--- server/src/router/models/models.ts
+++ server/src/router/models/models.ts
@@ -1,23 +1,17 @@
 import axios, { AxiosResponse } from "axios";
 import express, { Request, Response } from 'express';
+import { config } from "./config";
 import * as endpoints from "./endpoints";
 
 const router = express.Router();
 
-router.get("/models", async (req: Request, res: Response) => {
+router.get(endpoints.MODEL, async (req: Request, res: Response) => {
     try {
-    const config = {
-        method: 'get',  
-        url: endpoints.API,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
-    const response: AxiosResponse<any[]> = await axios(config);
-    const models = response.data;
-    const newArr = models.map((obj) => obj.title)
-    console.log("Models", newArr);
-    res.json(newArr);
+        const response: AxiosResponse<any[]> = await axios(config);
+        const models = response.data;
+        const newArr = models.map((obj) => obj.title)
+        console.log("Models", newArr);
+        res.json(newArr);
     } catch (e) {
         console.log("Error", e);
     }
